5a549b6998f56d8987f1c2172d5cb454defdab44
Jon Dewey <donjewey@gmail.com>
2022-08-22 19:09:04 -0700

    ts integration complete

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {App} from './App';
-import '../../public/styles.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import "../../public/styles.scss";
 
-const appElement = document.getElementById('app');
+const appElement = document.getElementById("app");
 
 ReactDOM.render(<App />, appElement);
