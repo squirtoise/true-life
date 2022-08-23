a5c5d81937983becd0f597ea0b4fcebe6f16f48d
Jon Dewey <donjewey@gmail.com>
2022-08-22 19:05:19 -0700

    integrated ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,76 @@
-const path = require("path");
-const HTMLWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, "src", "client", "index.js"),
+  entry: path.resolve(__dirname, 'client', 'index.tsx'),
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
   },
   mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
         test: /\.jsx?/,
-        exclude: "/node_modules/",
+        exclude: '/node_modules/',
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
       {
-        test:/\.html$/,
-        use: ['html-loader']
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.s[ac]ss$/i,
-        exclude: "/node_modules/",
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(jpg|png|jpeg)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "img/",
-              publicPath: "img/",
-            },
-          },
-        ],
+        exclude: '/node_modules/',
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(jpe?g|gif|png|svg)$/i,
         use: [
-        {
-          loader: 'url-loader',
-          options: {
-            limit: 10000
-          }
-        }
-      ]
-    },
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
-    new HTMLWebPackPlugin({ template: "./public/index.html" }),
+    new HTMLWebPackPlugin({ template: './public/index.html' }),
     new MiniCssExtractPlugin(),
   ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 
   devServer: {
+    // add all routes here
+    host: '0.0.0.0',
+    port: 8080,
+    hot: true,
+    historyApiFallback: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
     proxy: {
-      "/users": "http://localhost:3000",
+      '/api/*': 'http://localhost:3000'
     },
-    open: false,
   },
 };
