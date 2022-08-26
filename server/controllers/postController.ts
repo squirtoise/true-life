90745e8e0ebfa2de9cb5f255c293b7392c5d72d0
Jon Dewey <donjewey@gmail.com>
2022-08-25 19:17:42 -0700

    updates to postController

--- server/controllers/postController.ts
+++ server/controllers/postController.ts
@@ -1,26 +1,75 @@
-import express, { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
+import db from "../scripts/dbModel";
+import queries from "../scripts/dbQueries";
 
 const postController: any = {};
 
 // queries DB for all posts, saves returned post array to res.locals
-postController.all = (req: Request, res: Response, next: NextFunction) => {};
+postController.all = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const result: any = db.query(queries.getAllPosts);
+  res.locals.posts = result.rows;
+  return next();
+};
 
 // queries DB for a user's post, saves returned post array to res.locals
-postController.user = (req: Request, res: Response, next: NextFunction) => {};
+postController.user = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const result: any = db.query(queries.getUserPosts, [req.params.id]);
+  res.locals.posts = result.rows[0];
+  return next();
+};
 
-// queries DB for a user's friend's post, saves returned post array to res.locals
-postController.friend = (req: Request, res: Response, next: NextFunction) => {};
+// TEST THIS
+// queries DB for a user's friends' posts, saves returned post array to res.locals
+postController.friends = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const result: any = db.query(queries.getFriendPosts, [req.params.id]);
+  res.locals.posts = result.rows[0];
+  return next();
+};
 
 // queries DB for one post, saves returned post to res.locals
-postController.one = (req: Request, res: Response, next: NextFunction) => {};
+postController.one = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const result: any = db.query(queries.getPost, [req.params.id]);
+  res.locals.posts = result.rows[0];
+  return next();
+};
 
 // adds new post to DB, saves returned post to res.locals
-postController.new = (req: Request, res: Response, next: NextFunction) => {};
+postController.new = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+    
+};
 
 // updates post in DB, saves returned post to res.locals
-postController.put = (req: Request, res: Response, next: NextFunction) => {};
+postController.put = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
 
 // deletes post from DB
-postController.del = (req: Request, res: Response, next: NextFunction) => {};
+postController.del = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
 
 export default postController;
--- server/routes/postRouter.ts
+++ server/routes/postRouter.ts
@@ -1,41 +1,85 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+
+import postController from "../controllers/postController";
 
 const router = express.Router();
 
 // returns array of all posts
-router.get("/");
-
-// returns array of all a post's comments
-router.get("/comment/:id");
+router.get(
+  "/",
+  postController.all,
+  (req: Request, res: Response, next: NextFunction) => {
+    return res.locals.posts.length > 0
+      ? res.json(res.locals.posts)
+      : res.status(404).send("No posts found in DB");
+  }
+);
 
 // returns array of all a user's posts
-router.get("/user/:id");
+router.get(
+  "/user/:id",
+  postController.user,
+  (req: Request, res: Response, next: NextFunction) => {
+    return res.locals.posts.length > 0
+      ? res.json(res.locals.posts)
+      : res.status(404).send("No user posts found in DB");
+  }
+);
 
+// TEST THIS
 // returns array of all user's friends' posts
-router.get("/friends/:id");
+router.get(
+  "/friends/:id",
+  postController.friends,
+  (req: Request, res: Response, next: NextFunction) => {
+    return res.locals.posts.length > 0
+      ? res.json(res.locals.posts)
+      : res.status(404).send("No friend posts found in DB");
+  }
+);
 
 // returns a post
-router.get("/:id");
+// req.params.id : id of row in post table to return
+router.get("/:id", (req: Request, res: Response, next: NextFunction) => {
+  return res.locals.post > 0
+    ? res.json(res.locals.post)
+    : res.status(404).send("Post not found in DB");
+});
+
+// returns array of all a post's comments
+router.get(
+  "/comment/:id",
+  (req: Request, res: Response, next: NextFunction) => {}
+);
 
 // creates a post and returns new post
 // (id specified is user creating post)
-router.post("/:id");
+router.post("/:id", (req: Request, res: Response, next: NextFunction) => {});
 
 // creates a comment and returns new comment
 // (id specified is post to be commented on)
-router.post("/comment/:id");
+router.post(
+  "/comment/:id",
+  (req: Request, res: Response, next: NextFunction) => {}
+);
 
 // updates a post and returns updated post
-router.put("/:id");
+router.put("/:id", (req: Request, res: Response, next: NextFunction) => {});
 
 // updates a comment on a post
 // (id specified is of comment to be updated)
-router.put("/comment/:id");
+router.put(
+  "/comment/:id",
+  (req: Request, res: Response, next: NextFunction) => {}
+);
 
 // deletes a post
-router.delete("/:id");
+router.delete("/:id", (req: Request, res: Response, next: NextFunction) => {});
 
 // deletes a comment
-router.delete("/comment/:id");
+router.delete(
+  "/comment/:id",
+  (req: Request, res: Response, next: NextFunction) => {}
+);
 
 export default router;
