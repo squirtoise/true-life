7a6dc49e9bcd2d81986dbe7d4f6834ab5053c0e5
Jon Dewey <donjewey@gmail.com>
2022-08-25 20:32:40 -0700

    additions to server test suite

--- __test__/server/index.test.ts
+++ __test__/server/index.test.ts
@@ -1,12 +1,33 @@
-import app from "../../server/index";
+import app from "./testServer";
 import request from "supertest";
 
 // NODE_ENV set to test, uses test dB
 
+/* 
+  Test DB visual (after tests):
+
+  users:
+  userId: 1
+  userId: 2
+
+  user_friends:
+  userId: 1, friendId: 2, request: false
+  userId: 2, friendId: 1, request: false
+  (request = true on initial post, then request is accepted by user 2)
+
+  posts:
+  postId: 1, creator: 1
+  postId: 2, creator: 2
+
+  comments:
+  commentId: 1, postId: 1, creator: 1
+  commentId: 1, postId: 2, creator: 1
+*/
+
 describe("REST route tests", () => {
-  describe("Basic user functionality", () => {
+  describe("User routes", () => {
     // should create a new user successfully
-    test("POST: User", (done) => {
+    test.only("POST: User", async () => {
       let user = {
         email: "1",
         password: "1",
@@ -15,26 +36,50 @@ describe("REST route tests", () => {
         window: "01:00",
       };
 
-      request(app)
-        .post("/api/user")
-        .send(JSON.stringify(user))
-        .set("Accept", "application/json")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .expect(user)
-        .end((err, res) => {
-          if (err) return err;
-          return done();
-        });
-
-    // add another
-      user.email = "2";
-      request(app)
+      const user1 = await request(app)
         .post("/api/user")
         .send(JSON.stringify(user))
         .set("Accept", "application/json");
+
+      expect(user1.headers["Content-Type"]).toMatch(/json/);
+      expect(user1.status).toEqual(200);
+      expect(user1.body).toEqual(user);
+
+      // add another
+      user.email = "2";
+
+      const user2 = await request(app)
+        .post("/api/user")
+        .send(JSON.stringify(user))
+        .set("Accept", "application/json");
+
+      expect(user2.headers["Content-Type"]).toMatch(/json/);
+      expect(user2.status).toEqual(200);
+      expect(user2.body).toEqual(user);
     });
-    test("GET: User", () => {});
+
+    test("GET: User", async () => {
+      // user ID of first user created in GET: User
+      const userID = 1;
+
+      const expectedUser = {
+        email: "1",
+        password: "1",
+        first: "1",
+        last: "1",
+        window: "01:00",
+      };
+
+      const response = await request(app)
+        .get(`/api/user/${userID}`)
+        .set("Accept", "application/json");
+
+      expect(response.headers["Content-Type"]).toMatch(/json/);
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(expectedUser);
+
+      return;
+    });
   });
 });
 
--- __test__/server/userController.test.ts
+++
@@ -1,3 +0,0 @@
-describe("User Controller Tests", () => {});
-
-export {};
