d4d230df698496213622e8d8e5c7bfc8e09e324f
JeraldStephenson <62264085+JeraldStephenson@users.noreply.github.com>
2022-08-27 09:39:05 -0700

    finished menu dropdown when in mobile

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,51 +1,57 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 import {
-  NavbarContainer,
-  LeftContainer,
-  RightContainer,
-  NavbarInnerContainer,
-  NavbarExtendedContainer,
-  NavbarLinkContainer,
-  MenuButton,
-  CenterContainer,
-  NavbarLink,
-  TrueLifeMobileLink,
-  UserProfile,
+    NavbarContainer,
+    LeftContainer,
+    RightContainer,
+    NavbarInnerContainer,
+    NavbarExtendedContainer,
+    NavbarLinkContainer,
+    MenuButton,
+    CenterContainer,
+    NavbarLink,
+    NavbarLinkExtended,
+    TrueLifeMobileLink,
+    UserProfile,
 } from './styles/Navbar.style';
 
 export function Navbar() {
-  const [extendNavbar, setExtendNavbar] = useState<boolean>(false);
-  return (
-    <NavbarContainer>
-      <NavbarInnerContainer>
-        <LeftContainer>
-          <NavbarLinkContainer>
-            {/* Navbar links / buttons */}
+    const [extendNavbar, setExtendNavbar] = useState<boolean>(false);
+    return (
+        <NavbarContainer extendNavbar={extendNavbar}>
+            <NavbarInnerContainer>
+                <LeftContainer>
+                    <NavbarLinkContainer>
+                        {/* Navbar links / buttons */}
 
-            <NavbarLink to='/friends'> Friends </NavbarLink>
-            <NavbarLink to='/streak'>Streak </NavbarLink>
-            <NavbarLink to='/myProfile'> Profile </NavbarLink>
-            <MenuButton
-              onClick={() => {
-                setExtendNavbar((prev) => !prev);
-              }}
-            >
-              {extendNavbar ? <> &#10005; </> : <> &#8801; </>}
-            </MenuButton>
-          </NavbarLinkContainer>
-        </LeftContainer>
-        <CenterContainer>
-          <NavbarLink to='/'> TrueLife </NavbarLink>
-          <TrueLifeMobileLink to='/'>TL</TrueLifeMobileLink>
-        </CenterContainer>
-        <RightContainer>
-          {/* Profile Icon/Img */}
-          <UserProfile src='#'></UserProfile>
-        </RightContainer>
-      </NavbarInnerContainer>
-      <NavbarExtendedContainer></NavbarExtendedContainer>
-    </NavbarContainer>
-  );
+                        <NavbarLink to="/friends"> Friends </NavbarLink>
+                        <NavbarLink to="/streak">Streak </NavbarLink>
+                        <NavbarLink to="/myProfile"> Profile </NavbarLink>
+                        <MenuButton
+                            onClick={() => {
+                                setExtendNavbar((prev) => !prev);
+                            }}
+                        >
+                            {extendNavbar ? <> &#10005; </> : <> &#8801; </>}
+                        </MenuButton>
+                    </NavbarLinkContainer>
+                </LeftContainer>
+                <CenterContainer>
+                    <NavbarLink to="/"> TrueLife </NavbarLink>
+                    <TrueLifeMobileLink to="/">TL</TrueLifeMobileLink>
+                </CenterContainer>
+                <RightContainer>
+                    {/* Profile Icon/Img */}
+                    <UserProfile src="#"></UserProfile>
+                </RightContainer>
+            </NavbarInnerContainer>
+            {extendNavbar && (
+                <NavbarExtendedContainer>
+                    <NavbarLinkExtended to="/friends"> Friends </NavbarLinkExtended>
+                    <NavbarLinkExtended to="/streak">Streak </NavbarLinkExtended>
+                    <NavbarLinkExtended to="/myProfile"> Profile </NavbarLinkExtended>
+                </NavbarExtendedContainer>
+            )}
+        </NavbarContainer>
+    );
 }
--- client/components/styles/Navbar.style.tsx
+++ client/components/styles/Navbar.style.tsx
@@ -1,96 +1,115 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-// NavbarLink - for desktop view w/ react-router links ?
-export const NavbarLink = styled(Link)`
-  color: white;
-  font-size: x-large;
-  font-family: Arial, Helvetica, sans-serif;
-  text-decoration: none;
-  margin: 10px;
+interface NavbarContainerProps  {
+    extendNavbar: boolean;
+};
 
-  @media (max-width: 860px) {
-    display: none;
-  }
+export const NavbarLink = styled(Link)`
+    color: white;
+    font-size: x-large;
+    font-family: Arial, Helvetica, sans-serif;
+    text-decoration: none;
+    margin: 10px;
+
+    @media (max-width: 860px) {
+        display: none;
+    }
+`;
+
+export const NavbarLinkExtended = styled(Link)`
+    color: white;
+    font-size: x-large;
+    font-family: Arial, Helvetica, sans-serif;
+    text-decoration: none;
+    margin: 10px;
 `;
 export const TrueLifeMobileLink = styled(Link)`
-  width: 40px;
-  height: 40px;
-  color: white;
-  font-size: x-large;
-  font-family: Arial, Helvetica, sans-serif;
-  text-decoration: none;
-  margin: 10px;
+    width: 40px;
+    height: 40px;
+    color: white;
+    font-size: x-large;
+    font-family: Arial, Helvetica, sans-serif;
+    text-decoration: none;
+    margin: 10px;
 
-  @media (min-width: 861px) {
-    display: none;
-  }
+    @media (min-width: 861px) {
+        display: none;
+    }
 `;
 
 // profile img /icon
 export const UserProfile = styled.img`
-  border-color: white;
-  border: solid;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    border-color: white;
+    border: solid;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
-export const NavbarContainer = styled.nav`
-  width: 100%;
-  height: 80px;
-  background-color: black;
-  display: flex;
-  flex-direction: column;
+export const NavbarContainer = styled.nav<NavbarContainerProps>`
+    width: 100%;
+    height: ${(props) => (props.extendNavbar ? '30vh' : '80px')};
+    background-color: black;
+    display: flex;
+    flex-direction: column;
 `;
 
 export const LeftContainer = styled.div`
-  display: flex;
-  flex: 30%;
-  align-items: center;
-  padding-left: 5%;
-  background-color: black;
+    display: flex;
+    flex: 30%;
+    align-items: center;
+    padding-left: 5%;
+    background-color: black;
 `;
 export const CenterContainer = styled.div`
-  display: flex;
-  flex: 50%;
-  align-items: center;
-  justify-content: center;
-  color: white;
-  padding-right: 10%;
+    display: flex;
+    flex: 50%;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    padding-right: 10%;
 
-  background-color: black;
+    background-color: black;
 `;
 export const RightContainer = styled.div`
-  display: flex;
-  flex: 20%;
-  justify-content: flex-end;
-  padding-right: 50px;
-  background-color: black;
+    display: flex;
+    flex: 20%;
+    justify-content: flex-end;
+    padding-right: 50px;
+    background-color: black;
 `;
 
 export const NavbarInnerContainer = styled.div`
-  width: 100%;
-  height: 80px;
-  display: flex;
+    width: 100%;
+    height: 80px;
+    display: flex;
 `;
 
 export const NavbarLinkContainer = styled.div`
-  display: flex;
+    display: flex;
 `;
 export const MenuButton = styled.button`
-  width: 70px;
-  height: 50px;
-  background: none;
-  border: none;
-  color: white;
-  font-size: 45px;
-  cursor: pointer;
+    width: 70px;
+    height: 50px;
+    background: none;
+    border: none;
+    color: white;
+    font-size: 45px;
+    cursor: pointer;
 
-  @media (min-width: 861px) {
-    display: none;
-  }
+    @media (min-width: 861px) {
+        display: none;
+    }
 `;
 
-export const NavbarExtendedContainer = styled.div``;
+export const NavbarExtendedContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    @media (min-width: 861px) {
+        display: none;
+    }
+`;
