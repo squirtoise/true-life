9032a832b385db6e6c1eb30359031f6f95ce5b19
Ryan Hastie <rbhastie@gmail.com>
2022-08-31 19:57:23 -0400

    Start fetch"

--- client/components/pages/FeedPage.tsx
+++ client/components/pages/FeedPage.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Post from '../Post';
 
 function FeedPage() {
+
+    const [feed, setFeed] = React.useState([]);
+    
+    React.useEffect(() => {
+
+        const getFeed =  async () => {
+            let mounted = true;
+            try {
+                const response = await fetch('/');
+                const data = await response.json();
+                if (response.ok)
+                    if (mounted) {
+                        console.log("data.posts", data.posts);
+                        setFeed(data.posts);
+                    } else {
+                        console.error("component did not mount", data.error);
+                    };
+    
+            } catch(err) {
+                console.log(err);
+            }
+            return () => mounted = false;
+        }
+
+        getFeed();
+
+    }, []);
+
     return (
         <div>
             <h1>This is my Feed</h1>
@@ -12,6 +40,11 @@ function FeedPage() {
                     user="testUser"
                     timeStamp="8/26/2022 4:56AM"
                 />
+                <div>
+                    {feed.map((post, index) => {
+                        <Post key={index} user={post.user} timeStamp={post.timestamp} imageURI={post.image}/>
+                    })}
+                </div>
             </div>
         </div>
     );
